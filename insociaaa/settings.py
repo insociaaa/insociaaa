fd5bafc7997455e36a9481ff7cc953b118cc310b
insociaaa league <insociaaa@yahoo.com>
2014-04-02 21:13:59 +0530

    adding files

--- insociaaa/settings.py
+++ insociaaa/settings.py
@@ -36,6 +36,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_facebook',
+    'app_google',
+    'app_linkedin',
+    'app_twitter',
+    'app_yahoo',
 )
 
 MIDDLEWARE_CLASSES = (
