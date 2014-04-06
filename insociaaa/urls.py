eee5736242b9f2be5119fde54c98fa20710e0d59
insociaaa league <insociaaa@yahoo.com>
2014-04-06 10:12:58 +0530

    modifying urls.py

--- insociaaa/urls.py
+++ insociaaa/urls.py
@@ -7,7 +7,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'insociaaa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'insociaaa.views.index', name='index'),
+    url(r'^$', 'insociaaa.views.login', name='login'),
+    url(r'^login$', 'insociaaa.views.login', name='login'),
+    url(r'^facebook$', 'insociaaa.views.facebook', name='facebook'),
+    url(r'^google$', 'insociaaa.views.google', name='google'),
+    url(r'^linkedin$', 'insociaaa.views.linkedin', name='linkedin'),
+    url(r'^twitter$', 'insociaaa.views.twitter', name='twitter'),
+    url(r'^yahoo$', 'insociaaa.views.yahoo', name='yahoo'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
